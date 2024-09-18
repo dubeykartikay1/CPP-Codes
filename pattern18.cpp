c90c51585028ecd1ae4ed82b3ff9c5ad91154f6d
dubeykartikay1 <149271169+dubeykartikay1@users.noreply.github.com>
2024-09-18 19:54:09 +0530

    Add files via upload

---
+++ pattern18.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    for (int i = 0; i < n; i++)
+
+    {
+       for (char ch = 'E'-i; ch <= 'E'; ch++)
+       {
+        /* code */
+        cout<<ch<<" ";
+       }
+       cout<<endl;
+    }
+    
+}
